618ec44bcc49833f8f2eea21335b9411b2e7668e
Santi <jaramilloocamposantiago@gmail.com>
2023-03-01 17:02:31 -0500

    Feat: Complete and Delete ToDos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,34 +1,65 @@
-import { TodoCounter } from './components/TodoCounter';
-import { TodoControls } from './components/TodoControls';
-import { TodoSearch } from './components/TodoSearch';
-import { TodoList } from './components/TodoList';
-import { TodoItem } from './components/TodoItem';
-import './TodoApp.css'
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoControls } from "./components/TodoControls";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { TodoItem } from "./components/TodoItem";
+import "./TodoApp.css";
 import React from "react";
 
 const defaultTodoList = [
-  {text: 'Cortar cebolla', completed: true},
-  {text: 'Curso de intro a React', completed: false},
-  {text: 'Llorar', completed: false},
-  {text: 'Llorar2', completed: false},
-]
+  { text: "Cortar cebolla", completed: true },
+  { text: "Curso de intro a React", completed: false },
+  { text: "Llorar", completed: false },
+  { text: "Llorar2", completed: false },
+];
 
 function TodoApp() {
   const [searchValue, setSearchValue] = React.useState("");
   const [todoList, setTodoList] = React.useState(defaultTodoList);
-  const completedTodos = todoList.filter(todo => !!todo.completed).length;
+  const completedTodos = todoList.filter((todo) => !!todo.completed).length;
   const totalTodos = todoList.length;
-  const filteredResult = defaultTodoList.filter(todo => todo.text.includes(searchValue));
+
+  let filteredResult = [];
+
+  if (searchValue.length < 1) {
+    filteredResult = todoList;
+  } else {
+    filteredResult = todoList.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+
+  const completeTodo = (text) => {
+    const todoIndex = todoList.findIndex((todo) => todo.text == text);
+    const newTodoList = [...todoList];
+    newTodoList[todoIndex].completed = !newTodoList[todoIndex].completed;
+    setTodoList(newTodoList);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodoList = todoList.filter((todo) => todo.text !== text);
+    setTodoList(newTodoList);
+  };
+
   return (
-    <div className='TodoApp'>
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
-      <TodoControls/>
+    <div className="TodoApp">
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      <TodoControls />
       <TodoList>
-        {filteredResult.map(( item ) => (
-          <TodoItem key={item.text} text={item.text} completed={item.completed}/>
+        {filteredResult.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
-      <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos}/>
+      <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,25 +2,18 @@ import React from "react";
 import "./../styles/TodoItem.css";
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert("Listo completo")
-    }
-    const onDelete = () => {
-        alert("Listo borrado")
-    }
-
     return (
         <li className="TodoItem">
             <span
                 className={`check-icon ${props.completed && 'check-icon__completed'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
                 ✓
             </span>
             <p className={`${props.completed && 'completed'}`}>{props.text}</p>
             <span
                 className="delete-icon"
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
