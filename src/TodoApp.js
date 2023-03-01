23db6ff3845a6cee5ef7bcd5dcd388a8d45a3bc8
Santi <jaramilloocamposantiago@gmail.com>
2023-03-01 17:40:43 -0500

    Feat: Using Local Storage

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,16 +6,26 @@ import { TodoItem } from "./components/TodoItem";
 import "./TodoApp.css";
 import React from "react";
 
-const defaultTodoList = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Curso de intro a React", completed: false },
-  { text: "Llorar", completed: false },
-  { text: "Llorar2", completed: false },
-];
+// const defaultTodoList = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Curso de intro a React", completed: false },
+//   { text: "Llorar", completed: false },
+//   { text: "Llorar2", completed: false },
+// ];
 
 function TodoApp() {
+  const localStorageTodoList = localStorage.getItem("TODOLIST_V1");
+  let parsedTodoList;
+
+  if (!localStorageTodoList) {
+    localStorage.setItem("TODOLIST_V1", JSON.stringify([]));
+    parsedTodoList = [];
+  } else {
+    parsedTodoList = JSON.parse(localStorageTodoList);
+  }
+
   const [searchValue, setSearchValue] = React.useState("");
-  const [todoList, setTodoList] = React.useState(defaultTodoList);
+  const [todoList, setTodoList] = React.useState(parsedTodoList);
   const completedTodos = todoList.filter((todo) => !!todo.completed).length;
   const totalTodos = todoList.length;
 
@@ -24,24 +34,28 @@ function TodoApp() {
   if (searchValue.length < 1) {
     filteredResult = todoList;
   } else {
-    filteredResult = todoList.filter(todo => {
+    filteredResult = todoList.filter((todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
   }
 
+  const saveTodoList = (newTodoList) => {
+    setTodoList(newTodoList);
+    localStorage.setItem("TODOLIST_V1", JSON.stringify(newTodoList));
+  }
 
   const completeTodo = (text) => {
     const todoIndex = todoList.findIndex((todo) => todo.text == text);
     const newTodoList = [...todoList];
     newTodoList[todoIndex].completed = !newTodoList[todoIndex].completed;
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   const deleteTodo = (text) => {
     const newTodoList = todoList.filter((todo) => todo.text !== text);
-    setTodoList(newTodoList);
+    saveTodoList(newTodoList);
   };
 
   return (
@@ -49,7 +63,7 @@ function TodoApp() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoControls />
       <TodoList>
-        {filteredResult.map(todo => (
+        {filteredResult.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
